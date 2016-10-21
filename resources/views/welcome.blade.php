2f469ddf79b5e071ca3a43e3671555cda4adbcae
Ole Christian Ostlyng <olechristianostlyng@Denniss-iMac-2.local>
2016-10-21 13:37:06 +0200

    Endret litt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,6 +86,7 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
                 <p>Tester alias</p>
+                <p>En ny test</p>
             </div>
         </div>
     </body>
