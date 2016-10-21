8ad534c821df2734f83b11955894745e2b760fe9
Ole Christian Ostlyng <olechristianostlyng@Denniss-iMac-2.local>
2016-10-21 13:24:24 +0200

    Tester alias

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
-                <p>Her står det noe nå</p>
+                <p>Tester alias</p>
             </div>
         </div>
     </body>
